1e1673bfddefaa928876cab35e5bed173e9ecc1d
LePingu <hatim.nourbay@gmail.com>
2021-04-10 14:56:18 +0100

    Error in test powershell scripting file

--- Scripts/test.ps1
+++ Scripts/test.ps1
@@ -2,6 +2,6 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Write-host "My directory is $dir"
 Push-Location $dir/..
-Push-Location ./SimpleMemory
+Push-Location ./SimpleMemory.Tests
 dotnet build
-dotnet run
+dotnet test
